525f217c9f776758903c79c7b57c77534ea3f108
parvans <parvansajeevan@gmail.com>
2026-04-11 19:37:07 +0530

    updated

---
+++ app/(tabs)/subscriptions/[id].tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link, useLocalSearchParams } from 'expo-router'
+
+const SubscriptionDetails = () => {
+    const {id} = useLocalSearchParams<{id:string}>();
+
+  return (
+    <View>
+      <Text>SubscriptionDetails: {id}</Text>
+      <Link href="/">Go Back</Link>
+    </View>
+  )
+}
+
+export default SubscriptionDetails
