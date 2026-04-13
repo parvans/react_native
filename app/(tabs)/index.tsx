ad093b9571561dcde37093fec1c78a5abb39a16d
parvans <parvansajeevan@gmail.com>
2026-04-13 12:02:30 +0530

    Implement fonts on the app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,19 +10,20 @@ export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
 
-      <Text className="text-xl font-bold text-success font-mono">
-        Welcome to Nativewind!
+      <Text className="text-7xl text-success font-sans-extrabold">
+        Home
       </Text>
-      <Link href="/Onboarding" className="mt-4 text-lg text-accent">
+      
+      <Link href="/Onboarding" className="mt-4 font-sans text-lg text-accent">
         Go to Onboarding
       </Link>
-      <Link href="/(auth)/sign-in" className="mt-4 text-lg text-accent">
+      <Link href="/(auth)/sign-in" className="mt-4 font-sans text-lg text-accent">
         Go to Sign In
       </Link>
-      <Link href="/(auth)/sign-up" className="mt-4 text-lg text-accent">
+      <Link href="/(auth)/sign-up" className="mt-4 font-sans text-lg text-accent">
         Go to Sign Up
       </Link>
-      <Link href="/subscriptions/12332" className="mt-4 text-lg text-destructive">
+      <Link href="/subscriptions/12332" className="mt-4 font-sans text-lg text-destructive">
         Go to Subscription Details
       </Link>
       <Link href={{
